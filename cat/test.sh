d6be3d0e14ab72df49611dd5c898d7c2483569de
Harvey Speaker <hspeaker@student.21-school.ru>
2022-08-02 19:46:34 +0700

    delete lines

--- cat/test.sh
+++ cat/test.sh
@@ -1,46 +1,46 @@
 #!/bin/bash
 
 cat test_file_2.txt >> orig_cat.txt
-./s21_cat test_file_2.txt >> s21_cat.txt
-diff orig_cat.txt s21_cat.txt
+./_cat test_file_2.txt >> _cat.txt
+diff orig_cat.txt _cat.txt
 echo SUCCES TEST NO FLAG
 
 cat -b test_file_2.txt >> orig_cat.txt
-./s21_cat -b test_file_2.txt >> s21_cat.txt
-diff orig_cat.txt s21_cat.txt
+./_cat -b test_file_2.txt >> _cat.txt
+diff orig_cat.txt _cat.txt
 echo SUCCES TEST -b FLAG
 
 cat -b test_file_1.txt test_file_2.txt >> orig_cat.txt
-./s21_cat -b test_file_1.txt test_file_2.txt >> s21_cat.txt
-diff orig_cat.txt s21_cat.txt
+./_cat -b test_file_1.txt test_file_2.txt >> _cat.txt
+diff orig_cat.txt _cat.txt
 echo SUCCES TEST -b FLAG AND TWO FILES
 
 cat -e test_file_2.txt >> orig_cat.txt
-./s21_cat -e test_file_2.txt >> s21_cat.txt
-diff orig_cat.txt s21_cat.txt
+./_cat -e test_file_2.txt >> _cat.txt
+diff orig_cat.txt _cat.txt
 echo SUCCES TEST -e FLAG
 
 cat -n test_file_2.txt >> orig_cat.txt
-./s21_cat -n test_file_2.txt >> s21_cat.txt
-diff orig_cat.txt s21_cat.txt
+./_cat -n test_file_2.txt >> _cat.txt
+diff orig_cat.txt _cat.txt
 echo SUCCES TEST -n FLAG
 
 cat -s test_file_2.txt >> orig_cat.txt
-./s21_cat -s test_file_2.txt >> s21_cat.txt
-diff orig_cat.txt s21_cat.txt
+./_cat -s test_file_2.txt >> _cat.txt
+diff orig_cat.txt _cat.txt
 echo SUCCES TEST -s FLAG
 
 cat -t test_file_2.txt >> orig_cat.txt
-./s21_cat -t test_file_2.txt >> s21_cat.txt
-diff orig_cat.txt s21_cat.txt
+./_cat -t test_file_2.txt >> _cat.txt
+diff orig_cat.txt _cat.txt
 echo SUCCES TEST -t FLAG
 
 cat -v test_file_2.txt >> orig_cat.txt
-./s21_cat -v test_file_2.txt >> s21_cat.txt
-diff orig_cat.txt s21_cat.txt
+./_cat -v test_file_2.txt >> _cat.txt
+diff orig_cat.txt _cat.txt
 echo SUCCES TEST -v FLAG
 
 cat -benst test_file_2.txt >> orig_cat.txt
-./s21_cat -benst test_file_2.txt >> s21_cat.txt
-diff orig_cat.txt s21_cat.txt
+./_cat -benst test_file_2.txt >> _cat.txt
+diff orig_cat.txt _cat.txt
 echo SUCCES TEST -benst FLAG
